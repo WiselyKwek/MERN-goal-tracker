4168c2f665e6ec7ceaaa94828d7dc5b53c324011
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-05-02 15:25:19 +0800

    added route protection and user model and routes

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({extended: false}))
 
 // linking /api/goals to the goalRoutes file in routes folder
 app.use("/api/goals", require("./routes/goalRoutes"))
+app.use("/api/users", require("./routes/userRoutes"))
 
 // overrides the default error handler
 app.use(errorHandler)
